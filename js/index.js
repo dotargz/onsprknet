3a5fb8f3e0d0d8f093865625e934ec6b5301b9ba
BlueSkye <chamberlainsal@gmail.com>
2023-02-15 20:20:06 -0800

    add favicon, meta tags

--- js/index.js
+++ js/index.js
@@ -1,53 +1,52 @@
-
 // theme stuff
 themeCheck();
 
 function themeCheck() {
-  if (localStorage.getItem("theme") === null) {
-    applyLightTheme();
-  }
+	if (localStorage.getItem("theme") === null) {
+		applyLightTheme();
+	}
 
-  if (localStorage.getItem("theme") === "light") {
-    applyLightTheme();
-  }
+	if (localStorage.getItem("theme") === "light") {
+		applyLightTheme();
+	}
 
-  if (localStorage.getItem("theme") === "dark") {
-    applyDarkTheme();
-  }
+	if (localStorage.getItem("theme") === "dark") {
+		applyDarkTheme();
+	}
 }
 
 function applyLightTheme() {
-  let button = document.getElementById("themeButton");
-  let root = document.documentElement;
+	let button = document.getElementById("themeButton");
+	let root = document.documentElement;
 
-  localStorage.setItem("theme", "light");
+	localStorage.setItem("theme", "light");
 
-  root.style.setProperty("--bg-color", "#f0f0f0");
-  root.style.setProperty("--txt-color", "#333");
-  root.style.setProperty("--border-color", "#ccc");
-  root.style.setProperty("--less-color", "#aaa");
+	root.style.setProperty("--bg-color", "#f0f0f0");
+	root.style.setProperty("--txt-color", "#333");
+	root.style.setProperty("--border-color", "#ccc");
+	root.style.setProperty("--less-color", "#aaa");
 
-  button.innerHTML = ` <i class="fa-solid fa-moon"></i> `;
+	button.innerHTML = ` <i class="fa-solid fa-moon"></i> `;
 }
 
 function applyDarkTheme() {
-  let button = document.getElementById("themeButton");
-  let root = document.documentElement;
+	let button = document.getElementById("themeButton");
+	let root = document.documentElement;
 
-  localStorage.setItem("theme", "dark");
+	localStorage.setItem("theme", "dark");
 
-  root.style.setProperty("--bg-color", "#0f0f0f");
-  root.style.setProperty("--txt-color", "#ccc");
-  root.style.setProperty("--border-color", "#333");
-  root.style.setProperty("--less-color", "#555");
+	root.style.setProperty("--bg-color", "#0f0f0f");
+	root.style.setProperty("--txt-color", "#ccc");
+	root.style.setProperty("--border-color", "#333");
+	root.style.setProperty("--less-color", "#555");
 
-  button.innerHTML = ` <i class="fa-solid fa-sun"></i> `;
+	button.innerHTML = ` <i class="fa-solid fa-sun"></i> `;
 }
 
 function themeToggle() {
-  if (localStorage.getItem("theme") === "light") {
-    applyDarkTheme();
-  } else if (localStorage.getItem("theme") === "dark") {
-    applyLightTheme();
-  }
+	if (localStorage.getItem("theme") === "light") {
+		applyDarkTheme();
+	} else if (localStorage.getItem("theme") === "dark") {
+		applyLightTheme();
+	}
 }
--- js/quote.js
+++ js/quote.js
@@ -1,15 +1,13 @@
 getQuote();
 
 async function getQuote() {
-  let e = await getInspiration();
-  document.getElementById("quote").innerHTML = "\"" + e[0] + "\"";
-  document.getElementById("name").innerHTML = "- " + e[1];
+	let e = await getInspiration();
+	document.getElementById("quote").innerHTML = '"' + e[0] + '"';
+	document.getElementById("name").innerHTML = "- " + e[1];
 }
 
 async function getInspiration() {
-  const response = await fetch(
-    `https://one.npkn.net/quote-api/?api-key=8EosFBriByXLdt`
-  );
-  const data = await response.json();
-  return [data.quote, data.author];
+	const response = await fetch(`https://one.npkn.net/quote-api/?api-key=8EosFBriByXLdt`);
+	const data = await response.json();
+	return [data.quote, data.author];
 }
